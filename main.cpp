a8d1f0abc6b1ea6325849f95761536e34ed9da00
Edgariukus <90159467+Edgariukus@users.noreply.github.com>
2021-09-23 02:47:02 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@ struct studentas
     float nd[5];
     float egzas;
     float galutinis;
+    float mediana;
 };
+
 /*
 void print(studentas kint)
 {
@@ -20,12 +22,19 @@ void print(studentas kint)
 int main()
 {
     cout<<"Iveskite studentu skaiciu: "<<endl;
-    int n;
-    cin>>n;
+    int n,m;
+    cin>>n; //studentu skaicius
+    cout<<"Iveskite 1, noredami skaiciuoti studentu vidurkius, 2 - namu darbu medianas"<<endl;
+    cin>>m;
     studentas grupe[n];
     float sum=0;
     for(int i=0;i<n;i++)
     {
+        if(m!=1&&m!=2)
+        {
+            cout<<"Ivyko klaida: irasete netinkama simboli ar skaiciu"<<endl;
+            break;
+        }
         cout<<"Iveskite "<<i+1<<"-o studento varda ir pavarde: ";
         cin>>grupe[i].vardas>>grupe[i].pavarde;
         sum=0;
@@ -35,15 +44,42 @@ int main()
             cin>>grupe[i].nd[j];
             sum+=grupe[i].nd[j];
         }
+        for(int j=0;j<5;j++)
+        {
+            for(int k=j;k<5;k++)
+            {
+                if(grupe[i].nd[j]>grupe[i].nd[k])
+                {
+                    swap(grupe[i].nd[j],grupe[i].nd[k]);
+                }
+            }
+        }
+        if(5%2==1) for(int j=0;j<5;j++) grupe[i].mediana=grupe[i].nd[j/2];
+        else if(5%2==0) for(int j=0;j<5;j++) grupe[i].mediana=(grupe[i].nd[j/2]+grupe[i].nd[(j/2)-1])/2;
+
         cout<<"Iveskite "<<i+1<<"-o studento egzamino ivertinima: ";
         cin>>grupe[i].egzas;
         grupe[i].galutinis=0.4*sum/5+0.6*grupe[i].egzas;
     }
-    cout<<"Vardas"<<setw(20)<<"Pavarde"<<setw(20)<<"Galutinis (Vid.)"<<endl;
-    cout<<"-----------------------------------------------------------------"<<endl;
-    for(int i=0;i<n;i++)
+
+    if(m==1)
     {
-        cout<<grupe[i].vardas<<setw(20)<<grupe[i].pavarde<<setw(20)<<setprecision(2)<<grupe[i].galutinis<<endl;
+        cout<<"Vardas"<<setw(20)<<"Pavarde"<<setw(20)<<"Galutinis (Vid.)"<<endl;
+        cout<<"-----------------------------------------------------------------"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<grupe[i].vardas<<setw(20)<<grupe[i].pavarde<<setw(20)<<setprecision(2)<<grupe[i].galutinis<<endl;
+        }
+    }
+
+    else if(m==2)
+    {
+        cout<<"Vardas"<<setw(20)<<"Pavarde"<<setw(20)<<"Galutinis (Med.)"<<endl;
+        cout<<"-----------------------------------------------------------------"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<grupe[i].vardas<<setw(20)<<grupe[i].pavarde<<setw(20)<<setw(20)<<grupe[i].mediana<<endl;
+        }
     }
     //for(auto &kint:grupe) print(kint);
     return 0;
